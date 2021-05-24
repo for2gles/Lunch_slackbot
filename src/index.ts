e5587bea26b77709fd0cf0ce61c15176e5bdf110
Debian <bitnami@ip-172-26-13-94.ap-northeast-2.compute.internal>
2021-05-24 05:19:48 +0000

    goo

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,35 @@
-// src/index.ts
+// 업데이트 된 src/index.ts
 
 import express from 'express';
+import { WebClient } from '@slack/web-api';
 import { createEventAdapter } from '@slack/events-api';
 import { createServer } from 'http';
 
-// 생성한 슬랙봇에 대한 키값들
+// 생성한 슬랙앱에 대한 키값들
 import CONFIG from '../config/bot.json';
 /* 
   {
-    "SIGNING_SECRET": "XXXX"
+    "SIGNING_SECRET": "XXXX",
+    "BOT_USER_OAUTH_ACCESS_TOKEN": "xoxb-XXXX"
   }
  */
 
-// 슬랙에서 슬랙봇에게 접근가능한 엔드포인트를 만들기 위해 웹서버(express)를 사용
+// 슬랙에서 슬랙앱에게 접근가능한 엔드포인트를 만들기 위해 웹서버(express)를 사용
 const app = express();
 
 const slackEvents = createEventAdapter(CONFIG.SIGNING_SECRET);
+const webClient = new WebClient(CONFIG.BOT_USER_OAUTH_ACCESS_TOKEN);
 
 // 메시지 이벤트 구독하기
 slackEvents.on('message', async (event) => {
   console.log(event);
+
+  if (event.text == '?하이') {
+    webClient.chat.postMessage({
+      text: '안녕하세요!',
+      channel: event.channel,
+    });
+  }
 });
 
 // 메지지 이벤트 엔드포인트를 express 에 등록하기
